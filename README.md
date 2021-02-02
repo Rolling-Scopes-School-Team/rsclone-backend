936d9783509b859e68ab663a53a89ea044909998
Deeathwiing <andreykorotkins@gmail.com>
2021-02-02 14:13:11 +0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rsclone-backend
+
+### You need Docker for up server. 
+
+Use `npm start` to start and wait about 3 minutes.
--- mainServer/app.js
+++ mainServer/app.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import http from 'http';
-import config from './config.js';
-import { setUpConnection } from './src/init/dataBaseUtils.js';
-import InitLoaders from './src/loaders/index.js';
-import Chat from './src/chat/chat.js';
+import express from "express";
+import http from "http";
+import config from "./config.js";
+import { setUpConnection } from "./src/init/dataBaseUtils.js";
+import InitLoaders from "./src/loaders/index.js";
+import Chat from "./src/chat/chat.js";
+import { setTimeout } from "timers";
 
 const app = express();
 
-const server = http.createServer(app); 
- 
+const server = http.createServer(app);
+
 new InitLoaders(app).init();
 
 const connect = async () => {
@@ -21,6 +22,9 @@ const connect = async () => {
   await new Chat(server).init();
 };
 
-connect();
+setTimeout(
+  connect,
+  1000 * 60*3
+);
 
 export default server;
